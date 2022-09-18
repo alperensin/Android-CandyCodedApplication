0edfb407317971606f04f390b3b67a95cdb5ec8e
André Luiz Perensin <andreluizperensin@gmail.com>
2022-09-17 23:44:43 -0300

    added createPhoneIntent method to open dial

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -39,8 +39,11 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        Uri dialIntentDataUri = Uri.parse("tel:0123456789");
+        dialIntent.setData(dialIntentDataUri);
+        startActivity(dialIntent);
+    }
 
 }
